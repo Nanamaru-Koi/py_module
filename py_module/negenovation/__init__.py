abc7bebcda3e16eb828bcb6c83d31a67818e4da6
nanamaru-koi <nepnepnanakoi@gmail.com>
2022-05-24 19:39:48 +0900

    first commit

---
+++ py_module/negenovation/__init__.py
@@ -0,0 +1,4 @@
+from py_module.negenovation.ngiscan import readtxt,readNPY
+from py_module.negenovation.ngiimg import show_images,getIMG
+from py_module.negenovation.ngiana import myFFT, getPeak,moduletest
+from py_module.negenovation.ngitest import testcall
